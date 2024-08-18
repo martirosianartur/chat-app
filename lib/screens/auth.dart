09fb0b8cac2285f3e8b7818c5da559d390bd3d5e
martirosianartur <martirosianartur4@gmail.com>
2024-08-18 11:48:02 +0200

    bugfix

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -20,7 +20,7 @@ class _AuthScreenState extends State<AuthScreen> {
   void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid) {
+    if (!isValid) {
       return;
     }
     _form.currentState!.save();
